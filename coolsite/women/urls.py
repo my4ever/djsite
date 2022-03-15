bf71d7b903c764a8f8b1cab22c2fd1c409341fb0
my4ever <ruslan87yakimov@gmail.com>
2022-03-15 13:50:38 +0300

    lesson 15

--- coolsite/women/urls.py
+++ coolsite/women/urls.py
@@ -3,11 +3,11 @@ from .views import *
 
 
 urlpatterns = [
-    path('', index, name='index'),
+    path('', WomenHome.as_view(), name='index'),
     path('about/', about, name='about'),
-    path('addpage/', add_page, name='add_page'),
+    path('addpage/', WomenAdd.as_view(), name='add_page'),
     path('contact/', contact, name='contact'),
     path('login/', login, name='login'),
-    path('post/<slug:post_slug>/', show_post, name='post'),
-    path('category/<slug:cat_slug>/', show_category, name='category'),
+    path('post/<slug:post_slug>/', WomenDetail.as_view(), name='post'),
+    path('category/<slug:cat_slug>/', WomenCategory.as_view(), name='category'),
 ]
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -2,6 +2,8 @@ from django.http import HttpResponse, HttpResponseNotFound
 from django.core.handlers.wsgi import WSGIRequest
 from django.shortcuts import render, get_object_or_404, get_list_or_404, redirect
 from .forms import AddPostForm
+from django.views.generic import ListView, DetailView, CreateView
+
 
 from .models import *
 
@@ -11,16 +13,59 @@ menu = [{'title': 'О сайте', 'url_name': 'about'},
         {'title': 'Войти', 'url_name': 'login'}]
 
 
-def index(request: WSGIRequest) -> HttpResponse:
-    posts = Women.objects.all()
+class WomenHome(ListView):
+    # model = Women
+    queryset = Women.objects.filter(is_published=True)
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
 
-    context = {
-        'posts': posts,
-        'menu': menu,
-        'title': 'Главная страница',
-        'cat_selected': 0,
-    }
-    return render(request, 'women/index.html', context)
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Главная страница'
+        context['cat_selected'] = 0
+        return context
+
+
+class WomenCategory(ListView):
+    model = Women
+    template_name = 'women/index.html'
+    context_object_name = 'posts'
+    allow_empty = False
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Категория - ' + str(context['posts'][0].cat)
+        context['cat_selected'] = context['posts'][0].cat.id
+        return context
+
+    def get_queryset(self):
+        return Women.objects.filter(cat__slug=self.kwargs['cat_slug'], is_published=True)
+
+
+class WomenDetail(DetailView):
+    model = Women
+    template_name = 'women/post.html'
+    context_object_name = 'post'
+    slug_url_kwarg = 'post_slug'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = context['post'].title
+        return context
+
+
+class WomenAdd(CreateView):
+    form_class = AddPostForm
+    template_name = 'women/addpage.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['menu'] = menu
+        context['title'] = 'Добавить статью'
+        return context
 
 
 def about(request: WSGIRequest) -> render:
@@ -76,3 +121,15 @@ def show_category(request, cat_slug):
         'cat_selected': Category.objects.get(slug=cat_slug).id,
     }
     return render(request, 'women/index.html', context)
+
+
+# def index(request: WSGIRequest) -> HttpResponse:
+#     posts = Women.objects.all()
+#
+#     context = {
+#         'posts': posts,
+#         'menu': menu,
+#         'title': 'Главная страница',
+#         'cat_selected': 0,
+#     }
+#     return render(request, 'women/index.html', context)
